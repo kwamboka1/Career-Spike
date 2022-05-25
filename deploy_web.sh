9b44831784b14b36fc7811446b5393c9d0deaaba
kwamboka1 <makorimildred@gmail.com>
2022-05-25 15:47:06 +0300

    Create deploy_web.sh

---
+++ deploy_web.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Hello World!, Am happy learning to deploy my website! Yeey!" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+service nginx restart
+exit 0
