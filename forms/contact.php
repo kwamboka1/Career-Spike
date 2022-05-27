0fe5f4b02baaf93d6d13f81ad223e9eb5ead15a3
kwamboka1 <makorimildred@gmail.com>
2022-05-27 16:13:45 +0300

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,7 +1,7 @@
 <?php
 
   //our receiving email address
-  $receiving_email_address = 'contact@careerspike.com';
+  $receiving_email_address = 'makorimildred@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
